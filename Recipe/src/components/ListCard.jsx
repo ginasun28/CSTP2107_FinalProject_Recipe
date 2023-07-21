ada63a54199a7d905f373c819dcb705813641962
Gina Sun <ginasun28@gmail.com>
2023-07-21 01:41:31 -0700

    recipes page updated

--- Recipe/src/components/ListCard.jsx
+++ Recipe/src/components/ListCard.jsx
@@ -1,4 +1,9 @@
 /* eslint-disable react/prop-types */
+import {useEffect, useState} from "react";
+import useLocalStorage from "@/hooks/useLocalStorage.js";
+import {useNavigate} from "react-router-dom";
+import api from "@/api/index.js";
+import FavoriteModal from "@/components/FavoriteModa.jsx";
 import {
 	Typography,
 	Box,
@@ -6,10 +11,108 @@ import {
 	CardContent,
 	CardMedia,
 	Button,
+	IconButton,
 } from "@mui/material";
-import '../components/styles/ListCard.css'
+import {Delete as DeleteIcon, Edit as EditIcon} from "@mui/icons-material";
+import "../components/styles/ListCard.css";
 
-export default function LIstCard({data}) {
+export default function ListCard({product, isUser, load}) {
+	const [user, setUser] = useLocalStorage("user", null);
+
+	const navigate = useNavigate();
+	const [openModal, setOpenModal] = useState(false);
+	const [collections, setCollections] = useState([]);
+	const [selectedCollection, setSelectedCollection] = useState("");
+
+	useEffect(() => {
+		fetchCollections();
+	}, []);
+
+	const fetchCollections = async () => {
+		if (user) {
+			try {
+				const res = await api.getUserCollections(user.id);
+				setCollections(res.collections);
+			} catch (error) {
+				console.error("Failed to get user collections:", error);
+			}
+		}
+	};
+
+	// Function to handle closing the popup window
+	const handleCloseModal = () => {
+		setOpenModal(false);
+	};
+
+	// Function to handle selecting favorites
+	const handleSelectCollection = collectionId => {
+		setSelectedCollection(collectionId);
+	};
+
+	// Function that handles confirming favorites
+	const handleConfirmFavorite = async () => {
+		try {
+			// Add recipe to favorites
+			let {message} = await api.addFavorite({
+				userId: user.id,
+				collectionId: selectedCollection,
+				recipeId: product.id,
+			});
+			// close the popup
+			handleCloseModal();
+			// Prompt the user to save successfully
+			alert(message);
+		} catch (error) {
+			console.error("Failed to add favorite:", error);
+			alert("Failed to add favorite. Please try again later.");
+		}
+	};
+	const handleCreateCollection = newCollectionName => {
+		// Submits the new favorite name to the backend for creation
+		api
+			.createCollection({userId: user.id, name: newCollectionName})
+			.then(() => {
+				// After the creation is successful, reacquire the user's favorites list and refresh the favorites list
+				fetchCollections();
+			})
+			.catch(error => {
+				console.error("Error creating collection:", error);
+			});
+	};
+	const handleFavorite = () => {
+		if (!user) {
+			navigate("/signin");
+		}
+		api
+			.checkFavorite({
+				userId: user.id,
+				recipeId: product.id,
+			})
+			.then(async res => {
+				if (res.isFavorite) {
+					await api.removeFavorite(product.id, user.id);
+				} else {
+					setOpenModal(true);
+				}
+			});
+	};
+	const handleDeleteRecipe = async () => {
+		try {
+			// Call the api to delete the recipe
+			await api.deleteRecipes(product.id);
+			load();
+		} catch (error) {
+			console.error("Error deleting recipe:", error);
+			alert("Failed to delete recipe. Please try again later.");
+		}
+	};
+
+	const handleEditRecipe = () => {
+		// 编辑菜谱的逻辑，跳转到编辑菜谱的页面，需要根据具体的路由来实现
+		//The logic of editing recipes, jumping to the page of editing recipes, needs to be implemented according to specific routes
+		navigate(`/createRecipe?id=${product.id}`);
+	};
+
 	return (
 		<>
 			<Card
@@ -29,47 +132,87 @@ export default function LIstCard({data}) {
 							left: 0,
 							zIndex: 1,
 							padding: "10px",
-                            color:'#E38B29',
-                            background: 'linear-gradient(to left, rgba(253, 245, 202, 0.5), rgba(253, 245, 202, 4), rgba(253, 245, 202, 1))',
+							color: "#E38B29",
+							background:
+								"linear-gradient(to left, rgba(253, 245, 202, 0.5), rgba(253, 245, 202, 4), rgba(253, 245, 202, 1))",
 						}}
 					>
-						{data.tag}
+						{product.type}
 					</Typography>
 					<CardMedia
 						component="img"
 						sx={{width: 250, height: 250, flexShrink: 0}}
-						src={data.image}
-						alt="Recipe image"
+						src={product.image}
+						alt={product.name}
 					/>
 				</Box>
 				<Box sx={{display: "flex", flexDirection: "column", width: "100%"}}>
 					<CardContent sx={{flex: "1 0 auto"}}>
 						<Box className="card-save-container">
-							<img
-								src="/src/assets/icons8-save-96.png"
-								alt=""
-								className="card-icon"
-							/>
+							<IconButton
+								onClick={handleFavorite}
+								sx={{position: "absolute", top: "0px", right: "10px"}}
+							>
+								<img
+									src="/src/assets/icons8-save-96.png"
+									alt=""
+									className="card-icon"
+								/>
+							</IconButton>
 						</Box>
 						<Typography component="div" variant="h5">
-							{data.title}
+							{product.recipeName}
 						</Typography>
 						<Typography
 							variant="subtitle1"
 							color="text.secondary"
 							component="div"
 						>
-							{data.user}
+							{product.user?.username}
 						</Typography>
 						<Typography sx={{fontSize: "16px"}} className="card-desc">
-							{data.description}
+							{product.instructions}
 						</Typography>
 					</CardContent>
+					{isUser && (
+						<Box>
+							<IconButton
+								color="primary"
+								onClick={handleDeleteRecipe}
+								style={{position: "absolute", bottom: "10px", right: "50px"}}
+							>
+								<DeleteIcon />
+							</IconButton>
+							<IconButton
+								color="primary"
+								onClick={handleEditRecipe}
+								style={{position: "absolute", bottom: "10px", right: "10px"}}
+							>
+								<EditIcon />
+							</IconButton>
+						</Box>
+					)}
 					<Box sx={{textAlign: "center", position: "relative", top: "-20px"}}>
-						<Button className="read-more">Read More</Button>
+						<Button
+							onClick={() => {
+								navigate(`/recipes/${product.id}`);
+							}}
+							className="read-more"
+						>
+							Read More
+						</Button>
 					</Box>
 				</Box>
 			</Card>
+			<FavoriteModal
+				open={openModal}
+				onClose={handleCloseModal}
+				collections={collections}
+				selectedCollection={selectedCollection}
+				onCollectionSelect={handleSelectCollection}
+				onConfirmFavorite={handleConfirmFavorite}
+				onCreateCollection={handleCreateCollection}
+			/>
 		</>
 	);
 }
--- Recipe/src/pages/Recipes.jsx
+++ Recipe/src/pages/Recipes.jsx
@@ -1,80 +1,199 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import Navbar from "@/components/Navbar";
-import ProductList from "@/components/ProductList.jsx";
-import {FormControl, MenuItem, Select, TextField} from "@mui/material";
+import "../components/styles/RecipesList.css";
+import {
+	// FormControl,
+	// Select,
+	Box,
+	Container,
+	Grid,
+	TextField,
+	IconButton,
+	InputAdornment,
+	Typography,
+	Menu,
+	MenuItem,
+	Tooltip,
+} from "@mui/material";
+import {Search as SearchIcon} from "@mui/icons-material";
 import api from "@/api/index.js";
+import ListCard from "../components/ListCard";
+
+const types = [
+	"All",
+	"Breakfast",
+	"Lunch",
+	"Dinner",
+	"Desserts",
+	"Salad",
+	"Snack",
+	"Side Dishes",
+	"Soup",
+];
 
 const Recipes = () => {
-    const [products, setProducts] = useState([]);
-    const [searchText, setSearchText] = useState("");
-    const [filterType, setFilterType] = useState("");
+	const [anchorEl, setAnchorEl] = useState(null);
+	const [products, setProducts] = useState([]);
+	const [searchText, setSearchText] = useState("");
+	const [selectedFilter, setSelectedFilter] = useState("");
+	const [filteredRecipes, setFilteredRecipes] = useState([]);
 
-    useEffect(() => {
-        //Get the latest recipe list
-        api.getAllRecipe().then((res) => {
-                setProducts(res.latestRecipes)
-        });
-    }, []);
+	useEffect(() => {
+		//Get the latest recipe list
+		api.getAllRecipe().then(res => {
+			setProducts(res.latestRecipes);
+		});
+	}, []);
 
-    // Handle the search box text change event
-    const handleSearchTextChange = (event) => {
-        setSearchText(event.target.value);
-    };
+	// Handle the search box text change event
+	const handleSearchTextChange = event => {
+		setSearchText(event.target.value);
+	};
 
-    // Handle filter type change event
-    const handleFilterTypeChange = (event) => {
-        setFilterType(event.target.value);
-    };
+	// Handle the search button click event
+	const handleSearchButtonClick = () => {
+		api.search({keyword: searchText, filterType: selectedFilter}).then(res => {
+			setProducts(res);
+            console.log(res);
+		});
+	};
 
-    // Filter based on search text and filter type
-    const filteredProducts = products.filter((product) => {
-        return filterType === "" || product.type === filterType;
-    });
+	// Handles keyboard key events, and if the "Enter" key is pressed, perform a search operation
+	const handleKeyPress = event => {
+		if (event.key === "Enter") {
+			handleSearchButtonClick();
+		}
+	};
 
-    // Handle the search button click event
-    const handleSearchButtonClick = () => {
-        api.search({keyword: searchText, filterType: filterType}).then(res => {
-            setProducts(res);
-        })
-    };
+    // filter icon function
+	const handleFilterClick = event => {
+		setAnchorEl(event.currentTarget);
+	};
 
-    // Handles keyboard key events, and if the "Enter" key is pressed, perform a search operation
-    const handleKeyPress = (event) => {
-        if (event.key === "Enter") {
-            handleSearchButtonClick();
-        }
-    };
+	const handleCloseFilter = () => {
+		setAnchorEl(null);
+	};
 
-    return (
-        <div>
-            <Navbar/>
+	const handleFilterSelection = filter => {
+		setSelectedFilter(filter);
 
-            {/*search bar */}
-            <TextField
-                label="Search Recipe"
-                value={searchText}
-                onChange={handleSearchTextChange}
-                onKeyPress={handleKeyPress}
-                variant="outlined"
-                style={{margin: "10px"}}
-            />
+		// Filter the recipes based on the selected filter
+		const filteredRecipes = products.filter(recipe =>
+			filter === "All" ? true : recipe.type.includes(filter)
+		);
 
-            {/* filter dropdown list */}
-            <FormControl variant="outlined" style={{minWidth: 100, margin: "10px"}}>
-                <Select value={filterType} onChange={handleFilterTypeChange}>
-                    <MenuItem value="">All Types</MenuItem>
-                    <MenuItem value="Breakfast">Breakfast</MenuItem>
-                    <MenuItem value="Lunch">Lunch</MenuItem>
-                    <MenuItem value="Dinner">Dinner</MenuItem>
-                    <MenuItem value="Dessert">Dessert</MenuItem>
-                    <MenuItem value="Snack">Snack</MenuItem>
-                </Select>
-            </FormControl>
+		setFilteredRecipes(filteredRecipes);
+		setAnchorEl(null);
+	};
+    // End of filter icon function
 
-            {/* recipe list */}
-            <ProductList products={filteredProducts}/>
-        </div>
-    );
+	return (
+		<>
+			<Navbar />
+			{/* recipe list */}
+			<Container maxWidth="xl">
+				<Box sx={{padding: "45px 0px 0px 30px"}}>
+					<Grid>
+						<Grid item xs={6} sm={3} md={11}>
+							<Grid container spacing={2} alignItems="center">
+								<Grid
+									item
+									xs={4}
+									sm={6}
+									md={6}
+									className="filter-icon-container"
+								>
+									<TextField
+										label="Search"
+										variant="outlined"
+										value={searchText}
+										onChange={handleSearchTextChange}
+										onKeyPress={handleKeyPress}
+										sx={{width: 550}}
+										InputProps={{
+											sx: {borderRadius: "20px", height: 55},
+											endAdornment: (
+												<InputAdornment position="end">
+													<IconButton
+														aria-label="Search"
+														onClick={handleSearchButtonClick}
+													>
+														<SearchIcon />
+													</IconButton>
+												</InputAdornment>
+											),
+										}}
+									/>
+									<Box>
+										<Tooltip title="Find tags">
+											<IconButton
+												aria-label="Filter"
+												onClick={handleFilterClick}
+												classes={{root: "custom-icon-button"}}
+												disableRipple
+											>
+												<img
+													src="src/assets/icons8-filter-96 (2).png"
+													alt=""
+													style={{width: "35px", height: "35px"}}
+												/>
+											</IconButton>
+										</Tooltip>
+										<Box sx={{flexGrow: 0}}>
+											<Menu
+												sx={{mt: "5px"}}
+												id="menu-appbar"
+												anchorEl={anchorEl}
+												open={Boolean(anchorEl)}
+												onClose={handleCloseFilter}
+											>
+												{types.map(filter => (
+													<MenuItem
+														key={filter}
+														onClick={() => handleFilterSelection(filter)}
+														className="filter-icon"
+													>
+														<Typography textAlign="center" value={filter}>
+															{filter}
+														</Typography>
+													</MenuItem>
+												))}
+											</Menu>
+										</Box>
+									</Box>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Box>
+				<Grid container>
+					<Grid item xs={6} sm={3} md={11} className="content">
+						<Grid container spacing={2}>
+							{filteredRecipes.length > 0
+								? filteredRecipes.map(recipe => (
+										<Grid item xs={4} sm={6} md={6} key={recipe.id}>
+											{/* Pass individual recipe as a prop to ListCard */}
+											<ListCard product={recipe} />
+										</Grid>
+								))
+								: products.map(recipe => (
+										<Grid item xs={4} sm={6} md={6} key={recipe.id}>
+											{/* Pass individual recipe as a prop to ListCard */}
+											<ListCard product={recipe} />
+										</Grid>
+								))}
+						</Grid>
+					</Grid>
+					<Grid item xs={12} sm={6} className="half-trapezoid"></Grid>
+				</Grid>
+			</Container>
+		</>
+	);
 };
 
 export default Recipes;
+
+
+
+
+
